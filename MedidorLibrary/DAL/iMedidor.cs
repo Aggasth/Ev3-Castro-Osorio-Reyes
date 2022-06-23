1c832f9a7b56892ef27c1cf0695468cf8842269f
Aggasth <101593156+Aggasth@users.noreply.github.com>
2022-06-23 15:41:18 -0400

    Add files via upload

---
+++ MedidorLibrary/DAL/iMedidor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorLibrary.DAL
+{
+    public interface iMedidor
+    {
+        List<Medidor> ObtenerMedidores();
+        void AgregarMedidor(Medidor m);
+        bool Filtrar(Medidor m);
+        string TipoTxt(string t);
+    }
+}
