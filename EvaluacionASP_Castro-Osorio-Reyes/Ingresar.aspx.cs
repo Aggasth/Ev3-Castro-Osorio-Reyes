c66f461c47378293223d2c4d0824ea34e7b3da6e
Aggasth <101593156+Aggasth@users.noreply.github.com>
2022-06-23 15:40:49 -0400

    Add files via upload

---
+++ EvaluacionASP_Castro-Osorio-Reyes/Ingresar.aspx.cs
@@ -0,0 +1,49 @@
+﻿using MedidorLibrary.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EvaluacionASP_Castro_Osorio_Reyes
+{
+    public partial class Ingresar : System.Web.UI.Page
+    {
+        private iMedidor medidorDAL = new Medidor();
+        private iLectura lecturasDAL = new Lectura();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.fechaBox.Text = DateTime.Now.ToString("g");
+
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+            int idMedidor = Convert.ToInt32(this.txtId.Text.Trim());
+            double consumoMedidor = Convert.ToDouble(this.txtConsumo.Text.Trim());
+            string tipoMedidor = this.tiposRbl.SelectedValue;
+            DateTime fecha = Convert.ToDateTime(this.fechaBox.Text.Trim());
+            string tipo = medidorDAL.TipoTxt(tipoMedidor);
+            Medidor m = new Medidor
+            {
+                Id = idMedidor,
+                Consumo = consumoMedidor,
+                Tipo = tipo,
+                Fecha = fecha
+            };
+            bool flag = medidorDAL.Filtrar(m);
+            if (flag == true)
+            {
+                this.errorMsg.Text = "Este Medidor ya existe, intente nuevamente";
+            }
+            else
+            {
+                lecturasDAL.IngresarLectura(m);
+                this.successMsg.Text = "Medidor ingresado correctamente";
+            }
+            Response.Redirect("VerMedidores.aspx");
+        }
+
+    }
+}
---
+++ EvaluacionASP_Castro-Osorio-Reyes/VerMedidores.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="VerMedidores.aspx.cs" Inherits="EvaluacionASP_Castro_Osorio_Reyes.VerMedidores" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Contenido" runat="server">
+    <label>Ver Medidores</label>
+</asp:Content>
